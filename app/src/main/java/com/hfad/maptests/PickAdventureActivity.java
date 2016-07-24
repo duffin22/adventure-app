45524b5fc2c1a97a877389821a84ebe28d0c1d75
Matthew Duffin <matthewtduffin@gmail.com>
2016-07-23 19:22:21 -0700

    Initial commit

---
+++ app/src/main/java/com/hfad/maptests/PickAdventureActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.maptests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickAdventureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_adventure);
+    }
+}
